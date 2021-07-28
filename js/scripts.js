8b13268075531ccb91984d95106459b217bab8dc
Andy Mitchell <andymitchell1@live.co.uk>
2021-07-28 12:28:44 +0200

    Updated design and added JS code

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,40 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+let maValInput = document.getElementById("rawVal")
+maValInput.addEventListener("change",convertMaToSig)
+
+let sigValInput = document.getElementById("sigVal")
+sigValInput.addEventListener("change",convertSigToMa)
+
+let maMinInput = document.getElementById("maMin")
+maMinInput.addEventListener("change",convertMaToSig)
+let maMaxInput = document.getElementById("maMax")
+maMaxInput.addEventListener("change",convertMaToSig)
+
+let sigMinInput = document.getElementById("sigMin")
+sigMinInput.addEventListener("change",convertMaToSig)
+
+let sigMaxInput = document.getElementById("sigMax")
+sigMaxInput.addEventListener("change",convertMaToSig)
+
+function convertSigToMa(){
+    let maHigh = parseFloat(maMaxInput.value);
+    let maLow = parseFloat(maMinInput.value);
+    let sigHigh = parseFloat(sigMaxInput.value);
+    let sigLow = parseFloat(sigMinInput.value);
+    let sigValue = parseFloat(sigValInput.value);
+
+    maValInput.value = ((maHigh - maLow) / (sigHigh - sigLow)) * (sigValue - sigLow) + maLow;
+}
+
+function convertMaToSig(){
+    let sigHigh = parseFloat(sigMaxInput.value);
+    let sigLow = parseFloat(sigMinInput.value);
+    let maHigh = parseFloat(maMaxInput.value);
+    let maLow = parseFloat(maMinInput.value);
+    let maValue = parseFloat(maValInput.value);
+
+    sigValInput.value = ((sigHigh - sigLow) / (maHigh - maLow)) * (maValue - maLow) + sigLow;
+}
